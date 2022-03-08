f340cf0ce0e6bad68fe231af3ef982b8271abf43
Cole Rossman <colerossman99@gmail.com>
2022-03-08 14:39:32 -0800

    Main.css updates

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Main.css';
 import { signs } from '../../data.js';
 import Sign from '../Sign/Sign';
+import background from '../../space.png';
 
 export default function Main() {
-  return <main className='main'>
+  return <main className='main' style={{ backgroundImage: `url(${background})` }} >
     {signs.map((sign) => (
       <Sign key={sign.name} sign={sign} />
     ))};
