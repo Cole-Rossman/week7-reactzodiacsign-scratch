50138db40552261c029057d4bcc6e667eb1743f8
Cole Rossman <colerossman99@gmail.com>
2022-03-08 12:13:58 -0800

    data.js added with data

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const signs = [
+  {
+    name: 'Aquarius',
+    type: 'aquarius',
+    dates: 'Jan 21 - Feb 19',    
+  },
+  {
+    name: 'Aries',
+    type: 'aries',
+    dates: 'Mar 21 - Apr 19',    
+  },
+  {
+    name: 'Cancer',
+    type: 'cancer',
+    dates: 'Jun 21 - Jul 22',    
+  },
+  {
+    name: 'Capricorn',
+    type: 'capricorn',
+    dates: 'Dec 22 - Feb 19',    
+  },
+  {
+    name: 'Gemini',
+    type: 'gemini',
+    dates: 'May 21 - Jun 20',    
+  },
+  {
+    name: 'Leo',
+    type: 'leo',
+    dates: 'Jul 23 - Aug 22',    
+  },
+  {
+    name: 'Libra',
+    type: 'libra',
+    dates: 'Sept 23 - Oct 22',    
+  },
+  {
+    name: 'Pisces',
+    type: 'pisces',
+    dates: 'Feb 19 - Mar 20',    
+  },
+  {
+    name: 'Sagittarius',
+    type: 'sagittarius',
+    dates: 'Nov 22 - Dec 21',    
+  },
+  {
+    name: 'Scorpio',
+    type: 'scorpio',
+    dates: 'Oct 23 - Nov 21',    
+  },
+  {
+    name: 'Taurus',
+    type: 'taurus',
+    dates: 'Apr 20 - May 20',    
+  },
+  {
+    name: 'Virgo',
+    type: 'virgo',
+    dates: 'Aug 23 - Sep 22',    
+  },
+];
